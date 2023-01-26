541598da9cacc8c86ac40f7cfd25598bd2902085
Cassio Soares <cassiosoares9@hotmail.com>
2023-01-26 11:05:03 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -26,9 +26,7 @@ function App() {
 		try {
 			async function startFetching() {
 				const data = await axios.get(
-					`https://geo.ipify.org/api/v2/country,city?apiKey=${
-						import.meta.env.VITE_KEY
-					}&ipAddress=192.212.174.101`
+					`https://geo.ipify.org/api/v2/country,city?apiKey=${VITE_KEY}&ipAddress=192.212.174.101`
 				);
 				if (!ignore) {
 					setAddress(data.data);
@@ -47,7 +45,7 @@ function App() {
 
 	const getInputIP = async () => {
 		const data = await axios.get(
-			`https://geo.ipify.org/api/v2/country,city?apiKey=${import.meta.env.VITE_KEY}&${
+			`https://geo.ipify.org/api/v2/country,city?apiKey=${VITE_KEY}&${
 				checkIpAddress.test(ipAddress)
 					? `ipAddress=${ipAddress}`
 					: checkDomain.test(ipAddress)

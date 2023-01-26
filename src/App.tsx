1a5ea44a65fae255154749658fdaadb193e50762
Cassio Soares <cassiosoares9@hotmail.com>
2023-01-26 12:10:55 +0900

    vite.config change

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,9 @@ function App() {
 		try {
 			async function startFetching() {
 				const data = await axios.get(
-					`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.VITE_KEY}&ipAddress=192.212.174.101`
+					`https://geo.ipify.org/api/v2/country,city?apiKey=${
+						import.meta.env.VITE_KEY
+					}&ipAddress=192.212.174.101`
 				);
 				if (!ignore) {
 					setAddress(data.data);
@@ -45,7 +47,7 @@ function App() {
 
 	const getInputIP = async () => {
 		const data = await axios.get(
-			`https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.VITE_KEY}&${
+			`https://geo.ipify.org/api/v2/country,city?apiKey=${import.meta.env.VITE_KEY}&${
 				checkIpAddress.test(ipAddress)
 					? `ipAddress=${ipAddress}`
 					: checkDomain.test(ipAddress)
